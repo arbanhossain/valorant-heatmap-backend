7083cadf752719dd4bfcdc843e4d2a7f8adbba73
arbanhossain <arbanhossain@gmail.com>
2022-02-22 21:53:07 +0600

    jsonify import error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, render_template, request, redirect, Response, jsonify
 from models import MatchModel
 from Constants import *
 from utils import *
@@ -11,7 +11,7 @@ def match_data(match_id: str):
   if match_id == "c8d8bc50-c030-4abf-a27e-66b1c157429c":
     match_data = json.loads(open("example_split_game.json").read())
   match: MatchModel = MatchModel(match_data)
-  response = flask.jsonify({"players": match.get_players(), "kill_data": match.duel_data, "map_details": MAP[match.get_map()]})
+  response = jsonify({"players": match.get_players(), "kill_data": match.duel_data, "map_details": MAP[match.get_map()]})
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response
 
@@ -19,7 +19,7 @@ def match_data(match_id: str):
 def get_filtered_data():
   kill_data = request.get_json()["kill_data"]
   options = request.get_json()["options"]
-  response = flask.jsonify({ "data": filter_data(kill_data, options) })
+  response = jsonify({ "data": filter_data(kill_data, options) })
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response
 
@@ -27,7 +27,7 @@ def get_filtered_data():
 def get_plotted_image():
   kills = request.get_json()["data"]
   map_details = request.get_json()["map_details"]
-  response = flask.jsonify( {"data": plot_image(kills, map_details)} )
+  response = jsonify( {"data": plot_image(kills, map_details)} )
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response
 
