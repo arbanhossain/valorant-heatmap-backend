c963b282ce904da453adab6b9ec905401b0c7f61
arbanhossain <arbanhossain@gmail.com>
2022-04-07 15:13:08 +0600

    aded icebox in map list

--- Constants.py
+++ Constants.py
@@ -9,5 +9,16 @@ MAP: dict = {
       "bottom_left": { "x": -806, "y": -256 },
       "bottom_right": { "x": 1767, "y": -256 },
     }
+  },
+  "/Game/Maps/Port/Port": {
+    "id": "port",
+    "name": "Icebox",
+    "multipliers": {"x": 1/5, "y": -1/5},
+    "bounding_box": {
+      "top_left": { "x": -806, "y": 2400 },
+      "top_right": { "x": 1767, "y": 2400 },
+      "bottom_left": { "x": -806, "y": -256 },
+      "bottom_right": { "x": 1767, "y": -256 },
+    }
   }
 }
